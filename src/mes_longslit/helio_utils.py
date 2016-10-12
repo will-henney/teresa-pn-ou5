0359f1985aea9a512957212f365a4d027f11b72c
William Henney <will@henney.org>
2016-10-11 22:35:06 -0500

    Whole load of changes from the last year

--- src/mes_longslit/helio_utils.py
+++ src/mes_longslit/helio_utils.py
@@ -7,13 +7,20 @@ from astropy import constants as const
 from astropy.wcs import WCS
 from pyslalib.slalib import sla_dcs2c, sla_evp, sla_rverot, sla_obs
 
+OBSERVATORY_ALIASES = {
+    'SPM': 'SANPM83',
+    'VLT': 'VLT1',
+}
+
 def ra_dec_from_header(hdr):
+    """This trusts the nominal RA, Dec given in the header"""
     ra = coord.Longitude(hdr['RA'], u.hour)
     dec = coord.Latitude(hdr['DEC'], u.deg)
     return ra, dec
 
-def ra_dec_from_header_wcs(hdr):
-    w = WCS(hdr, key='A').celestial
+def ra_dec_from_header_wcs(hdr, wcskey):
+    """This uses the RA, Dec of the WCS reference pixel"""
+    w = WCS(hdr, key=wcskey).celestial
     ra = coord.Longitude(w.wcs.crval[0], u.deg)
     dec = coord.Latitude(w.wcs.crval[1], u.deg)
     return ra, dec
@@ -21,25 +28,41 @@ def ra_dec_from_header_wcs(hdr):
 def mjd_from_header(hdr):
     return float(hdr.get('MJD-OBS'))
 
-def obs_lat_SPM():
-    obs_id, obs_name, obs_long, obs_lat, obs_height = sla_obs(0, "SANPM83")
+def obs_lat_from_name(observatory):
+    if observatory in OBSERVATORY_ALIASES:
+        obs_string = OBSERVATORY_ALIASES[observatory]
+    else:
+        obs_string = observatory
+    obs_id, obs_name, obs_long, obs_lat, obs_height = sla_obs(0, obs_string)
     return coord.Latitude(obs_lat, u.radian)
 
 def st_from_header(hdr):
-    return coord.Longitude(hdr['ST'], u.hour)
+    if 'ST' in hdr:
+        # SPM observations have ST measured in hours
+        st = coord.Longitude(hdr['ST'], u.hour)
+    elif 'LST' in hdr:
+        # MUSE observations have LST measured in seconds
+        st = coord.Longitude(hdr['LST'], u.hour)/3600.0
+    else:
+        return KeyError('Neither ST nor LST found in header')
+    return st
 
-def helio_topo_from_header(hdr):
-    ra, dec = ra_dec_from_header_wcs(hdr)
+def helio_topo_from_header(hdr, usewcs=None, observatory='SPM'):
+    if usewcs is None:
+        ra, dec = ra_dec_from_header(hdr)
+    else:
+        # Set usewcs='' to use default WCS
+        ra, dec = ra_dec_from_header_wcs(hdr, wcskey=usewcs)
     mjd = mjd_from_header(hdr)
     st = st_from_header(hdr)
-    obs_lat = obs_lat_SPM()
+    obs_lat = obs_lat_from_name(observatory)
     return helio_topo_correction(ra, dec, mjd, st, obs_lat)
 
 def helio_topo_correction(ra, dec, mjd, st, obs_lat):
     """Find radial velocity correction in km/s due to transformation
     between topocentric and heliocentric frame.  Positive when
     observatory is moving away from source in heliocentric frame.
- 
+
     Parameters
     ----------
     ra : :class:`~astropy.coordinates.Longitude` 
@@ -73,24 +96,26 @@ def geo_topo_correction(ra, dec, st, obs_lat):
 
 
 LIGHT_SPEED_KMS = const.c.to('km/s').value
-def vels2waves(vels, restwav, hdr):
+def vels2waves(vels, restwav, hdr, usewcs=None, observatory='SPM'):
     """Heliocentric radial velocity (in km/s) to observed wavelength (in
     m, or whatever units restwav is in)
 
     """
     # Heliocentric correction
-    vels = np.array(vels) + helio_topo_from_header(hdr)
+    vels = np.array(vels) + helio_topo_from_header(hdr, usewcs=usewcs,
+                                                   observatory=observatory)
     waves = restwav*(1.0 + vels/LIGHT_SPEED_KMS)
     return waves
 
 
-def waves2vels(waves, restwav, hdr):
-    """Observed wavelength to Heliocentric radial velocity (in m/s) 
+def waves2vels(waves, restwav, hdr, usewcs=None, observatory='SPM'):
+    """Observed wavelength to Heliocentric radial velocity (in km/s) 
 
     """
     vels = const.c*(waves - restwav)/restwav
     # Heliocentric correction
-    vels -= helio_topo_from_header(hdr)*u.km/u.s
+    vels -= helio_topo_from_header(hdr, usewcs=usewcs,
+                                   observatory=observatory)*u.km/u.s
 
     return vels
 # Module\ to\ find\ heliocentric\ correction:\ helio_utils\.py:1 ends here

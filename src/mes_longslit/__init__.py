9760ee9c2a968fc93a1d279583c8107f4daf6db2
William Henney <will@henney.org>
2021-11-01 14:07:26 -0600

    Set up conda python environment and flit for package install

---
+++ src/mes_longslit/__init__.py
@@ -0,0 +1,10 @@
+"""
+Reduction of echelle longslit data from the Manchester Echelle
+Spectrograph (MES, or Mezcal), installed on the 2.1m telescope at
+Observatorio Astronómico Nacional, San Pedro Mártir (OAN-SPM
+Observatory, Mexico)
+
+William Henney, 2019, 2020, 2021
+"""
+
+__version__ = "0.1"

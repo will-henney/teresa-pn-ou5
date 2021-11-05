485e4c0c5c72653206eeb9578a8ebae5f7d6aa02
William Henney <will@henney.org>
2021-11-04 21:39:37 -0600

    Finish the median images

--- notebooks/01-00-image-astrometry.py
+++ notebooks/01-00-image-astrometry.py
@@ -48,7 +48,7 @@ o3_list = [_["image_id"] for _ in db if _.get("line_id") == "O III"]
 c0 = SkyCoord("21 14 20.03 +43 41 36.0", unit=(u.hourangle, u.deg))
 c0
 
-# We only need a small image around the center, since it is a small nebula.  We take 256 pixel square with 0.2 arcsec per pixel, so 51.2 arcsec along each side. The smallest pixels in the original data are 0.35 arcsec, so we are oversampling enough to hopefully avoid any aliasing issues.
+# We only need a small image around the center, since it is a small nebula.  We take 400 pixel square with 0.2 arcsec per pixel, so 80 arcsec along each side. The smallest pixels in the original data are 0.35 arcsec, so we are oversampling enough to hopefully avoid any aliasing issues.
 
 ha_dict = {}
 with warnings.catch_warnings():
@@ -60,7 +60,7 @@ with warnings.catch_warnings():
             hdulist, 
             center=c0, 
             pixscale=0.2 * u.arcsec,
-            shape=(256, 256),
+            shape=(400, 400),
         )    
 
 ha_dict
@@ -75,7 +75,7 @@ with warnings.catch_warnings():
             hdulist, 
             center=c0, 
             pixscale=0.2 * u.arcsec,
-            shape=(256, 256),
+            shape=(400, 400),
         )  
 o3_dict
 
@@ -104,7 +104,7 @@ ax.imshow(hdu.data, cmap="gray_r", origin="lower", vmin=-5, vmax=12)
 # * `bg_windows` is a list of the 4 corners
 
 # +
-peak_window = slice(100, 150), slice(100, 150)
+peak_window = slice(170, 230), slice(170, 230)
 bg_windows = [
     (slice(None, 50), slice(None, 50)),
     (slice(-50, None), slice(None, 50)),
@@ -125,18 +125,63 @@ def normalize(im):
 
 # -
 
+# Apply the normalization to the Ha and [O III] images
+
+for hdulist in ha_dict.values():
+    hdulist[0].data = normalize(hdulist[0].data)
+
+for hdulist in o3_dict.values():
+    hdulist[0].data = normalize(hdulist[0].data)
+
+# Make median images:
+
+ha_median = np.median(
+    np.stack(
+        [_[0].data for _ in ha_dict.values()],
+    ),
+    axis=0,
+)
+o3_median = np.median(
+    np.stack(
+        [_[0].data for _ in o3_dict.values()],
+    ),
+    axis=0,
+)
+
+# Look at all the Ha images
+
 fig, axes = plt.subplots(
     3, 4, sharex=True, sharey=True,
     figsize=(12, 9),
 )
 for ax, (name, hdulist) in zip(axes.flat, ha_dict.items()):
     hdu = hdulist[0]
-    ax.imshow(normalize(hdu.data), cmap="gray_r", origin="lower", vmin=-0.2, vmax=2.0)
+    ax.imshow(hdu.data, cmap="gray_r", origin="lower", vmin=-0.2, vmax=3.0)
     ax.set_title(name)
-axes[-1, -1].remove()
+axes[-1, -1].imshow(ha_median, cmap="gray_r", origin="lower", vmin=-0.2, vmax=3.0)
+axes[-1, -1].set_title("Median", fontweight="bold")
 fig.suptitle("PN Ou 5 – Ha exposures")
 fig.tight_layout()
 fig.savefig("ha-image-slit-mosaic.pdf")
+
+# They all look good.  The slit is clearly narrower in the first 3 images.
+#
+# Now write out the normalized images:
+
+# +
+for name, hdulist in ha_dict.items():
+    savefile = outpath / f"ha-imslit-{name}.fits"
+    hdulist.writeto(savefile, overwrite=True)
+
+# And the median image
+savefile = outpath / f"ha-imslit-median.fits"
+fits.PrimaryHDU(
+    header=hdulist[0].header,
+    data=ha_median,
+).writeto(savefile, overwrite=True)
+# -
+
+# Repeat for [O III]
 
 fig, axes = plt.subplots(
     2, 3, sharex=True, sharey=True,
@@ -144,21 +189,27 @@ fig, axes = plt.subplots(
 )
 for ax, (name, hdulist) in zip(axes.flat, o3_dict.items()):
     hdu = hdulist[0]
-    ax.imshow(normalize(hdu.data), cmap="gray_r", origin="lower", vmin=-0.2, vmax=2.0)
+    ax.imshow(normalize(hdu.data), cmap="gray_r", origin="lower", vmin=-0.2, vmax=3.0)
     ax.set_title(name)
-axes[-1, -1].remove()
+axes[-1, -1].imshow(o3_median, cmap="gray_r", origin="lower", vmin=-0.2, vmax=3.0)
+axes[-1, -1].set_title("Median", fontweight="bold")
 fig.suptitle("PN Ou 5 – [O III] exposures")
 fig.tight_layout()
 fig.savefig("o3-image-slit-mosaic.pdf")
 
+# +
+for name, hdulist in o3_dict.items():
+    savefile = outpath / f"o3-imslit-{name}.fits"
+    hdulist.writeto(savefile, overwrite=True)
+    
+# And the median image
+savefile = outpath / f"o3-imslit-median.fits"
+fits.PrimaryHDU(
+    header=hdulist[0].header,
+    data=o3_median,
+).writeto(savefile, overwrite=True)
+# -
 
 
-
-
-w
-
-tform = ax.get_transform("icrs")
-
-tform.get_matrix()
 
 

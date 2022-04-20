44a22569eea6afbf5c5d0698bbfde4b240c850a0
William Henney <will@henney.org>
2022-04-19 22:06:27 -0500

    Make 1D spectra plots of all the lines
    Also, do Gaussian fits to oiii and ha.  Do a zoomed in plot to show
    the high-velocity wings

--- notebooks/04-01-coadd-slits.py
+++ notebooks/04-01-coadd-slits.py
@@ -77,7 +77,12 @@ mes.convert_pv_offset_vels(c0.ra.deg, c0.dec.deg, line_id="ha", verbose=True)
 
 mes.convert_pv_offset_vels(c0.ra.deg, c0.dec.deg, line_id="oiii", verbose=True)
 
+# + tags=[]
 mes.convert_pv_offset_vels(c0.ra.deg, c0.dec.deg, line_id="heii", verbose=True)
+
+# + tags=[]
+mes.convert_pv_offset_vels(c0.ra.deg, c0.dec.deg, line_id="nii", verbose=True)
+# -
 
 pvpath = dpath / "pv-offset-vels"
 
@@ -337,6 +342,205 @@ hdu.writeto(pvpath2 / "ha-pv-coadd.fits", overwrite=True)
 
 
 # -
+# ## Repeat for He II and [N II]
+
+# +
+file_list = sorted(pvpath.glob("*-heii-*.fits"), key=fkey)
+
+N = len(file_list)
+ncols = 4
+nrows = (N // ncols) + 1
+fig = plt.figure(figsize=(4 * ncols, 8 * nrows))
+
+vsys = -33
+v1, v2 = vsys - 100, vsys + 100
+s1, s2 = -35, 35
+
+kernel = Gaussian2DKernel(x_stddev=2.0)
+for i, filepath in enumerate(file_list):
+    hdu, = fits.open(filepath)
+    w = WCS(hdu.header)
+    ax = plt.subplot(nrows, ncols, i + 1, projection=w)
+    xlims, ylims = w.world_to_pixel_values([v1, v2], [s1, s2])
+    y1, y2 = [int(_) for _ in ylims]
+    bg1 = np.median(hdu.data[y1-10:y1], axis=0)
+    bg2 = np.median(hdu.data[y2:y2+10], axis=0)
+    im = hdu.data - 0.5 * (bg1 + bg2)
+    im /= im.max()
+    ax.imshow(im, vmin=-0.1, vmax=1.0)
+    ims = convolve_fft(im, kernel)
+    ax.contour(
+        ims, 
+        levels=[0.005, 0.01, 0.02, 0.04, 0.08], 
+        colors="w",
+        linewidths=[0.5, 1.0, 1.5, 2.0, 2.5],
+    )
+
+    ax.set(xlim=xlims, ylim=ylims)
+    ax.set_title(filepath.stem)
+...;
+# -
+
+for filepath in file_list:
+    hdu, = fits.open(filepath)
+    hdu2 = mes.regrid_pv(hdu)
+    filepath2 = pvpath2 / f"{filepath.stem}-regrid.fits" 
+    hdu2.writeto(filepath2, overwrite=True)
+
+# +
+file_list = sorted(pvpath2.glob("*-heii-*.fits"), key=fkey)
+
+N = len(file_list)
+ncols = 4
+nrows = (N // ncols) + 1
+fig = plt.figure(figsize=(6 * ncols, 8 * nrows))
+
+vsys = -33
+v1, v2 = vsys - 100, vsys + 100
+s1, s2 = -35, 35
+
+kernel = Gaussian2DKernel(x_stddev=4.0)
+for i, filepath in enumerate(file_list):
+    hdu, = fits.open(filepath)
+    w = WCS(hdu.header)
+    ax = plt.subplot(nrows, ncols, i + 1, projection=w)
+    xlims, ylims = w.world_to_pixel_values([v1, v2], [s1, s2])
+    y1, y2 = [int(_) for _ in ylims]
+    x1, x2 = [int(_) for _ in xlims]
+    x0, y0 = w.world_to_pixel_values(0.0, 0.0)
+    x0 = int(x0)
+    bg1 = np.median(hdu.data[y1-10:y1], axis=0)
+    bg2 = np.median(hdu.data[y2:y2+10], axis=0)
+    im = hdu.data - 0.5 * (bg1 + bg2)
+    scale = np.percentile(im[y1:y2, x1:x0], 99)
+    im /= scale
+    ims = convolve_fft(im, kernel)
+    ax.imshow(ims, vmin=-0.1, vmax=1.0)
+    #ax.contour(
+    #    ims, 
+    #    levels=[0.005, 0.01, 0.02, 0.04, 0.08], 
+    #    colors="w",
+    #    linewidths=[0.5, 1.0, 1.5, 2.0, 2.5],
+    #)
+
+    ax.set(xlim=xlims, ylim=ylims)
+    ax.set_title(filepath.stem + f"\nweight = {hdu.header['WEIGHT']:.3f}")
+    ax.text(x1 + 5, y1 + 5, f"{i}", color="w", fontsize="x-large")
+...;
+
+# +
+selected = 2, 4, 5, 6, 7, 8, 9
+imlist = []
+weightsum = 0.0
+
+for isel in selected:
+    hdu, = fits.open(file_list[isel])
+    weight =  hdu.header["WEIGHT"]
+    imlist.append(weight * hdu.data)
+    weightsum += weight
+    
+hdu.data = np.sum(np.stack(imlist, axis=0), axis=0) / weightsum
+hdu.writeto(pvpath2 / "heii-pv-coadd.fits", overwrite=True)
+
+# +
+file_list = sorted(pvpath.glob("*-nii-*.fits"), key=fkey)
+
+N = len(file_list)
+ncols = 4
+nrows = (N // ncols) + 1
+fig = plt.figure(figsize=(4 * ncols, 8 * nrows))
+
+vsys = -33
+v1, v2 = vsys - 100, vsys + 100
+s1, s2 = -35, 35
+
+kernel = Gaussian2DKernel(x_stddev=2.0)
+for i, filepath in enumerate(file_list):
+    hdu, = fits.open(filepath)
+    w = WCS(hdu.header)
+    ax = plt.subplot(nrows, ncols, i + 1, projection=w)
+    xlims, ylims = w.world_to_pixel_values([v1, v2], [s1, s2])
+    y1, y2 = [int(_) for _ in ylims]
+    bg1 = np.median(hdu.data[y1-10:y1], axis=0)
+    bg2 = np.median(hdu.data[y2:y2+10], axis=0)
+    im = hdu.data - 0.5 * (bg1 + bg2)
+    im /= im.max()
+    ax.imshow(im, vmin=-0.1, vmax=1.0)
+    ims = convolve_fft(im, kernel)
+#    ax.contour(
+#        ims, 
+#        levels=[0.005, 0.01, 0.02, 0.04, 0.08], 
+#        colors="w",
+#        linewidths=[0.5, 1.0, 1.5, 2.0, 2.5],
+#    )
+
+    ax.set(xlim=xlims, ylim=ylims)
+    ax.set_title(filepath.stem)
+...;
+# -
+
+for filepath in file_list:
+    hdu, = fits.open(filepath)
+    hdu2 = mes.regrid_pv(hdu)
+    filepath2 = pvpath2 / f"{filepath.stem}-regrid.fits" 
+    hdu2.writeto(filepath2, overwrite=True)
+
+# +
+file_list = sorted(pvpath2.glob("*-nii-*.fits"), key=fkey)
+
+N = len(file_list)
+ncols = 4
+nrows = (N // ncols) + 1
+fig = plt.figure(figsize=(6 * ncols, 8 * nrows))
+
+vsys = -33
+v1, v2 = vsys - 100, vsys + 100
+s1, s2 = -35, 35
+
+kernel = Gaussian2DKernel(x_stddev=4.0)
+for i, filepath in enumerate(file_list):
+    hdu, = fits.open(filepath)
+    w = WCS(hdu.header)
+    ax = plt.subplot(nrows, ncols, i + 1, projection=w)
+    xlims, ylims = w.world_to_pixel_values([v1, v2], [s1, s2])
+    y1, y2 = [int(_) for _ in ylims]
+    x1, x2 = [int(_) for _ in xlims]
+    x0, y0 = w.world_to_pixel_values(0.0, 0.0)
+    x0 = int(x0)
+    bg1 = np.median(hdu.data[y1-10:y1], axis=0)
+    bg2 = np.median(hdu.data[y2:y2+10], axis=0)
+    im = hdu.data - 0.5 * (bg1 + bg2)
+    scale = np.percentile(im[y1:y2, x1:x0], 99.9)
+    im /= scale
+    ims = convolve_fft(im, kernel)
+    ax.imshow(ims, vmin=-0.1, vmax=1.0)
+    #ax.contour(
+    #    ims, 
+    #    levels=[0.005, 0.01, 0.02, 0.04, 0.08], 
+    #    colors="w",
+    #    linewidths=[0.5, 1.0, 1.5, 2.0, 2.5],
+    #)
+
+    ax.set(xlim=xlims, ylim=ylims)
+    ax.set_title(filepath.stem + f"\nweight = {hdu.header['WEIGHT']:.3f}")
+    ax.text(x1 + 5, y1 + 5, f"{i}", color="w", fontsize="x-large")
+...;
+
+# +
+selected = 2, 4, 5, 6, 7, 8, 9
+imlist = []
+weightsum = 0.0
+
+for isel in selected:
+    hdu, = fits.open(file_list[isel])
+    weight =  hdu.header["WEIGHT"]
+    imlist.append(weight * hdu.data)
+    weightsum += weight
+    
+hdu.data = np.sum(np.stack(imlist, axis=0), axis=0) / weightsum
+hdu.writeto(pvpath2 / "nii-pv-coadd.fits", overwrite=True)
+# -
+
 # ## Look at our co-added spectra
 
 
@@ -362,10 +566,22 @@ for i, filepath in enumerate(file_list):
     ax = plt.subplot(nrows, ncols, i + 1, projection=w)
     xlims, ylims = w.world_to_pixel_values([v1, v2], [s1, s2])
     y1, y2 = [int(_) for _ in ylims]
+    x1, x2 = [int(_) for _ in xlims]
+    x0, y0 = w.world_to_pixel_values(0.0, 0.0)
+    x0 = int(x0)
     bg1 = np.median(hdu.data[y1-10:y1], axis=0)
     bg2 = np.median(hdu.data[y2:y2+10], axis=0)
     im = hdu.data - 0.5 * (bg1 + bg2)
-    im /= im.max()
+    scale = np.percentile(im[y1:y2, x1:x0], 99.99)
+    im /= scale
+    # Save a FITS file of BG-subtracted and normalized image
+    fits.PrimaryHDU(
+        header=hdu.header,
+        data=im,
+    ).writeto(
+        str(filepath).replace(".fits", "-bgsub.fits"),
+        overwrite=True,
+    )
     ax.imshow(im, vmin=-0.1, vmax=1.0)
     ims = convolve_fft(im, kernel)
     ax.contour(
@@ -400,7 +616,284 @@ for i, filepath in enumerate(file_list):
 #
 #
 
+# ## Look at 1D spectra
+#
+# We can use the bg-subtracted files that we have just saved. Plot the Ha and oiii on the same graph. Show a series of spatial ranges:
 
+# +
+file_list = sorted(pvpath2.glob("*-pv-coadd-bgsub.fits"))
+
+linehdus = {
+    filepath.stem.split("-")[0]: fits.open(filepath)[0]
+    for filepath in file_list
+}
+
+
+positions = (
+    ("N knot", [20.0, 30.0]),
+    ("N outer", [9.0, 15.0]),
+    ("N inner", [3.0, 9.0]),
+    ("Core", [-3.0, 3.0]),
+    ("S inner", [-9.0, -3.0]),    
+    ("S outer", [-15.0, -9.0]),
+    ("S knot", [-30.0, -20.0]),
+)
+
+nlines = len(file_list)
+npos = len(positions)
+# -
+
+linehdus
+
+# +
+fig, axes = plt.subplots(
+    npos, 
+    1, 
+    sharex=True,
+    figsize=(12, 16),
+)
+
+vsys = -33
+v1, v2 = vsys - 100, vsys + 100
+
+skip = ["heii", "nii"]
+for ax, [pos_label, [s1, s2]] in zip(axes, positions):
+    for line_label, hdu in linehdus.items():
+        w = WCS(hdu.header)
+        ns, nv = hdu.data.shape
+        xlims, ylims = w.world_to_pixel_values([v1, v2], [s1, s2])
+        x1, x2 = [int(_) for _ in xlims]
+        y1, y2 = [int(_) for _ in ylims]
+        spec = hdu.data[y1:y2, x1:x2].mean(axis=0)
+        vels, _ = w.pixel_to_world_values(np.arange(nv), [0]*nv)
+        vels = vels[x1:x2]
+        if line_label == "nii":
+            spec[np.abs(vels) <= 10.0] = np.nan
+        if line_label == "heii":
+            spec[vels >= 30.0] = np.nan
+        if line_label in skip and np.abs([s1, s2]).min() > 4:
+            spec[:] = np.nan
+        ax.plot(vels, spec, label=line_label)
+    ax.axhline(0.0, linestyle="dashed", c="k", lw=1,)
+    ax.axvline(vsys, linestyle="dashed", c="k", lw=1,)
+    axtitle = f"  {pos_label}: [${int(s1):+d}$, ${int(s2):+d}$]"
+    ax.set_title(axtitle, loc="left", y=0.7)
+axes[3].legend(ncol=2)
+axes[-1].set(
+    xlabel="Heliocentric velocity, km/s",
+)
+fig.savefig("ou5-coadd-1dspec-all.pdf")
+...;
+# -
+
+# That version has all the lines for the brighter parts.  But, to be honest, they do not really add anything. 
+
+from astropy.modeling import models, fitting
+
+
+def mark_component(model, color, ax):
+    v = model.mean.value
+    a = model.amplitude.value
+    ax.plot([v, v], [0.3 * a, 0.7 * a], lw=3, color=color, alpha=0.7)
+
+
+# +
+fig, axes = plt.subplots(
+    npos, 
+    1, 
+    sharex=True,
+    figsize=(12, 16),
+)
+
+vsys = -33
+v1, v2 = vsys - 100, vsys + 100
+fitter = fitting.LevMarLSQFitter()
+keep = ["ha", "oiii"]
+gfits = {}
+for ax, [pos_label, [s1, s2]] in zip(axes, positions):
+    for line_label, hdu in linehdus.items():
+        if not line_label in keep:
+            continue
+        w = WCS(hdu.header)
+        ns, nv = hdu.data.shape
+        xlims, ylims = w.world_to_pixel_values([v1, v2], [s1, s2])
+        x1, x2 = [int(_) for _ in xlims]
+        y1, y2 = [int(_) for _ in ylims]
+        spec = hdu.data[y1:y2, x1:x2].mean(axis=0)
+        vels, _ = w.pixel_to_world_values(np.arange(nv), [0]*nv)
+        vels = vels[x1:x2]
+        dataline, = ax.plot(vels, spec, label=line_label)
+        c = dataline.get_color()
+        
+        # Fit two Gaussians
+        mask1 = vels < vsys
+        mask2 = ~mask1
+        sm1 = spec[mask1].max()
+        vm1 = vels[spec[mask1].argmax()]
+        sm2 = spec[mask2].max()
+        vm2 = vels[spec[mask2].argmax() + mask1.sum()]
+        g1 = models.Gaussian1D(amplitude=sm1, mean=vm1, stddev=10.0)
+        g2 = models.Gaussian1D(amplitude=sm2, mean=vm2, stddev=10.0)
+        if "knot" in pos_label:
+            init_model = g1
+            fac = -0.5
+        elif "outer" in pos_label:
+            init_model = g1 + g2
+            fac = 0.02           
+        else:
+            init_model = g1 + g2
+            fac = 0.3
+        fitmask = spec > fac * spec.max()
+        fitted_model = fitter(init_model, vels[fitmask], spec[fitmask])
+        gfits[(line_label, pos_label)] = fitted_model
+#        print(pos_label, line_label)
+#        print(init_model)
+#        print(fitted_model)
+        ax.plot(
+            vels, 
+            fitted_model(vels), 
+            linestyle="dashed", 
+            lw=2, 
+            c=c,
+            label=f"{line_label} fit",
+        )
+        if "knot" in pos_label:
+            mark_component(fitted_model, c, ax)
+        else:
+            mark_component(fitted_model[0], c, ax)
+            mark_component(fitted_model[1], c, ax)
+
+    ax.axhline(0.0, linestyle="dashed", c="k", lw=1,)
+    ax.axvline(vsys, linestyle="dashed", c="k", lw=1,)
+    axtitle = f"  {pos_label}: [${int(s1):+d}$, ${int(s2):+d}$]"
+    ax.set_title(axtitle, loc="left", y=0.7)
+axes[3].legend(ncol=2)
+axes[-1].set(
+    xlabel="Heliocentric velocity, km/s",
+)
+fig.savefig("ou5-coadd-1dspec-ha-oiii.pdf")
+...;
+# -
+
+# Now zoom the y axis, to look at the wings, but just for the inner regions:
+
+# + tags=[]
+cpos = {
+    k: v for k, v in positions
+    if np.abs(v).min() < 4
+}
+cpos
+
+
+# -
+
+def mark_component_low(model, color, ax):
+    v = model.mean.value
+    a = model.amplitude.value
+    ax.plot([v, v], [0.03, 0.07], lw=3, color=color, alpha=0.7)
+
+
+from astropy.convolution import convolve_models
+
+# +
+fig, axes = plt.subplots(
+    len(cpos), 
+    1, 
+    sharex=True,
+    figsize=(12, 16),
+)
+
+vsys = -33
+v1, v2 = vsys - 100, vsys + 100
+fitter = fitting.LevMarLSQFitter()
+keep = ["ha", "oiii"]
+gfits2 = {}
+for ax, [pos_label, [s1, s2]] in zip(axes, cpos.items()):
+    for line_label, hdu in linehdus.items():
+        if not line_label in keep:
+            continue
+        w = WCS(hdu.header)
+        ns, nv = hdu.data.shape
+        xlims, ylims = w.world_to_pixel_values([v1, v2], [s1, s2])
+        x1, x2 = [int(_) for _ in xlims]
+        y1, y2 = [int(_) for _ in ylims]
+        spec = hdu.data[y1:y2, x1:x2].mean(axis=0)
+        vels, _ = w.pixel_to_world_values(np.arange(nv), [0]*nv)
+        vels = vels[x1:x2]
+        # Fit two Gaussians
+        mask1 = vels < vsys
+        mask2 = ~mask1
+        sm1 = spec[mask1].max()
+        vm1 = vels[spec[mask1].argmax()]
+        sm2 = spec[mask2].max()
+        vm2 = vels[spec[mask2].argmax() + mask1.sum()]
+        g1 = models.Gaussian1D(amplitude=sm1, mean=vm1, stddev=10.0)
+        g2 = models.Gaussian1D(amplitude=sm2, mean=vm2, stddev=10.0)
+        g1b = models.Gaussian1D(amplitude=0.1 * sm1, mean=vm1, stddev=3.0)
+        g2b = models.Gaussian1D(amplitude=0.1 * sm2, mean=vm2, stddev=3.0)
+
+        init_model = g1 + g2 #+ g1b + g2b
+        fitmask = spec > 0.3 * spec.max()
+        fitted_model = fitter(init_model, vels[fitmask], spec[fitmask])
+        #fitted_model = fitter(init_model, vels, spec)
+        gfits2[(line_label, pos_label)] = fitted_model
+
+
+
+        dataline, = ax.plot(
+            vels, 
+            spec - fitted_model(vels), 
+            label=f"{line_label} residuals",
+        )
+        c = dataline.get_color()
+        ax.fill_between(vels, 0.0, spec, color=c, alpha=0.1, label=f"{line_label} observed")
+        ax.plot(
+            vels, 
+            fitted_model(vels), 
+            linestyle="dashed", 
+            lw=2, 
+            c=c,
+            label=f"{line_label} fit",
+        )
+        for component in fitted_model[:2]:
+            mark_component_low(component, c, ax)
+        for component in fitted_model:
+            ax.plot(
+                vels, 
+                component(vels), 
+                linestyle="dotted", 
+                lw=2, 
+                c=c,
+                alpha=0.5,
+            )
+
+           
+
+    ax.axhline(0.0, linestyle="dashed", c="k", lw=1,)
+    ax.axvline(vsys, linestyle="dashed", c="k", lw=1,)
+    axtitle = f"  {pos_label}: [${int(s1):+d}$, ${int(s2):+d}$]"
+    ax.set_title(axtitle, loc="left", y=0.7)
+    ax.set(ylim=[-0.04, 0.25])
+axes[1].legend(ncol=1)
+axes[-1].set(
+    xlabel="Heliocentric velocity, km/s",
+)
+fig.savefig("ou5-coadd-1dspec-wings.pdf")
+...;
+# -
+
+import pandas as pd
+
+m = gfits[("oiii", "Core")]
+dict(zip(m.param_names, m.parameters))
+
+pd.DataFrame(
+    {k: dict(zip(m.param_names, m.parameters)) for k, m in gfits.items()}
+).T
+
+pd.DataFrame(
+    {k: dict(zip(m.param_names, m.parameters)) for k, m in gfits2.items()}
+).T
 
 # ## Exploratory material
 
